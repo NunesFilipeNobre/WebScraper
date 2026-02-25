669e5c0219d57ee5837fe3d65f58a1fba52f08c7
Filipe Nunes <nunesfilipenobre@gmail.com>
2026-02-25 19:01:49 -0300

    feat: Initial commit

---
+++ webcrawler.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main(void) {
+    CURL *curl;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    curl = curl_easy_init();
+    if (!curl) {
+        fprintf(stderr, "Erro ao iniciar curl\n");
+        return 1;
+    }
+
+    curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+    res = curl_easy_perform(curl);
+
+    if (res != CURLE_OK) {
+        fprintf(stderr, "Erro: %s\n", curl_easy_strerror(res));
+    }
+
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+
+    return 0;
+}

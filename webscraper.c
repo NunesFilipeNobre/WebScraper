42b1bf65d00c9cef727fe0acf25c9b7659242c04
Filipe Nunes <nunesfilipenobre@gmail.com>
2026-03-02 19:28:35 -0300

    docs: Adding comments

--- webscraper.c
+++ webscraper.c
@@ -2,26 +2,27 @@
 #include <curl/curl.h>
 
 int main(void) {
-    CURL *curl;
+    CURL *curl;     //Defines a CURL type pointer to be used as a handle in the future
     CURLcode res;
 
-    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl_global_init(CURL_GLOBAL_DEFAULT);  //Initializes libcurl resources
 
-    curl = curl_easy_init();
+    curl = curl_easy_init();    //Initializes the handle to the transfers
     if (!curl) {
         fprintf(stderr, "Erro ao iniciar curl\n");
         return 1;
     }
 
-    curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    //Sets up the handle's options
+    curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); //Enforce a long int
 
-    res = curl_easy_perform(curl);
+    res = curl_easy_perform(curl);  //Performs the easy transfer
 
     if (res != CURLE_OK) {
         fprintf(stderr, "Erro: %s\n", curl_easy_strerror(res));
     }
 
-    curl_easy_cleanup(curl);
-    curl_global_cleanup();
+    curl_easy_cleanup(curl); //Closes handle
+    curl_global_cleanup();  //Cleans libcurl resources from memory
 }
